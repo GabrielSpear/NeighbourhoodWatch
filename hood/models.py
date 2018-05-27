bbcde1b2d1ec3a618aa095b369ffe22a5ca9296e
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 20:37:03 +0300

    import of user,group, reverse and htmlfields

--- hood/models.py
+++ hood/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 # Create your models here.
 
