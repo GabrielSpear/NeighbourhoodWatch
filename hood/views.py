071a939a46c4bca45fdc8154c9b30494e96470f5
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 18:26:24 +0300

    User profile setup function

--- hood/views.py
+++ hood/views.py
@@ -10,3 +10,13 @@ def index(request):
     profile_form = NewProfileForm
     # profile = Profile.objects.all()
     return render(request, 'index.html', {"post": post, "profile_form": NewProfileForm, "hood": hood})
+
+
+@login_required(login_url='/accounts/register')
+def profile(request, user_id):
+    current_user = request.user
+    profile = Profile.objects.all()
+    post = Post.objects.filter(profile__id=user_id)
+    # post_profile=Post.objects.get(id=user_id)
+
+    return render(request, 'all-watch/profile.html', {"current_user": current_user, "profile": profile, "post": post})
