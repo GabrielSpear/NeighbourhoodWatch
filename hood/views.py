893950bcb2ba804cf5fb654867208e9695e328e7
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 18:24:58 +0300

    Register function added

--- hood/views.py
+++ hood/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+@login_required(login_url='/accounts/register')
+def index(request):
+    post = Post.get_post()
+    hood = Hood.get_hood()
+    profile_form = NewProfileForm
+    # profile = Profile.objects.all()
+    return render(request, 'index.html', {"post": post, "profile_form": NewProfileForm, "hood": hood})
