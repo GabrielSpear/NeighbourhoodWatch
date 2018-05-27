fcb8b29e0e8e333ed54a43466a521af14eca9dff
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 18:35:15 +0300

    Added new post & hoodname urls

--- hood/urls.py
+++ hood/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^business/', views.business, name='business'),
     url(r'^post/', views.post, name='post'),
+    url(r'^create/post', views.new_post, name="new-post"),
+    url(r'^hood/', views.hood, name='hood'),
 
 ]
