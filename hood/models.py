c828ae222854cf509050a20a0f6c93068debcc1c
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 20:28:02 +0300

    Models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GroupExtend(models.Model):
+    group = models.OneToOneField(Group)
+
+    def __str__(self):
+        return self.group
+
+    def create_group(self):
+        self.save
+
+    def delete_group(self):
+        self.delete
