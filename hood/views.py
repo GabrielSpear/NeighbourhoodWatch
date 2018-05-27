d86aa6e2ff438db5b88c6a0cf6d7c3e1d63742d2
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 18:28:06 +0300

    import

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . forms import NewProfileForm, NewPostForm, NewHoodForm
 
 
 # Create your views here.
