44aae73f844ad43422f6e0f85dfb46067f448206
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-27 20:32:16 +0300

    Business models

--- hood/models.py
+++ hood/models.py
@@ -50,3 +50,34 @@ class Hood(models.Model):
     def update_count(cls):
         count = cls.objects.all()
         return count
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=90)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
+    email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def create_business(self):
+        self.save
+
+    def delete_business(self):
+        self.delete
+
+    @classmethod
+    def get_business(self):
+        business = Business.objects.all()
+        return business
+
+    @classmethod
+    def searched_business(cls, search_term):
+        biz = cls.objects.filter(user__username__icontains=search_term)
+        return biz
+
+    @classmethod
+    def update_business(cls):
+        update = cls.objects.all()
+        return update
