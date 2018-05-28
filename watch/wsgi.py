6a1021d64ca6ab696bb23bc862c57ecef32281b5
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-28 16:42:29 +0300

    Final changes

--- watch/wsgi.py
+++ watch/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import signal
+import sys
+import traceback
+import time
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "watch.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
