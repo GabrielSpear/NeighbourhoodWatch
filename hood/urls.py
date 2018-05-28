02412e21fb0e8e05c4eb2ab1b61eb4fa5f851e7b
Gabriel Gatumu <gabrieldvjspear@gmail.com>
2018-05-28 14:13:15 +0300

    media

--- hood/urls.py
+++ hood/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^update/hood', views.update_hood, name="update_hood"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
